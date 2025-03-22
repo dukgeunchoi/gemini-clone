6c753e690e2001950ad85bb612aa09d56a3826ff
Dukgeun Choi <dukgeun@Dukgeuns-MacBook-Pro.local>
2025-03-22 15:03:04 +1100

    added inital sidebar implementation

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import menu_icon from './menu.png'
+import help_icon from './question.png'
+import activity_icon from './restore.png'
+import add_icon from './add.png'
+import setting_icon from './settings.png'
+import chat_icon from './conversation.png'
+
+export const assets = {
+    menu_icon,
+    help_icon,
+    activity_icon,
+    add_icon,
+    setting_icon,
+    chat_icon
+}
